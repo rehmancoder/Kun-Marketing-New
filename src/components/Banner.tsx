9314a9b24326478edd43566a03b8044b2556b950
Rehman Shabbir <90275811+rehmancoder@users.noreply.github.com>
2023-10-01 22:45:23 +0500

    issues Done

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -17,10 +17,19 @@ const Banner: React.FC = () => {
               <h1 className="text-white md:text-4xl text-2xl text-center px-4 mt-5">
                 We have more than 18 years of experience
               </h1>
-              <Link
-                href="/article"
-                className="flex justify-center md:flex-row flex-col gap-6 md:gap-12 md:mt-20 mt-3"
-              >
+              <div className="flex justify-center md:flex-row flex-col gap-6 md:gap-12 md:mt-20 mt-3">
+                <Link href="/article" className="bg-white w-full h-56">
+                  <Image
+                    src={bannerCardImg}
+                    className="w-full h-[180px] object-cover"
+                    alt={""}
+                  />
+                  <div className="bg-primary w-full h-[44px] flex items-center justify-center">
+                    <span className="text-white tracking-wider font-Poppins">
+                      CAPITAL HILLS
+                    </span>
+                  </div>
+                </Link>
                 <div className="bg-white w-full h-56">
                   <Image
                     src={bannerCardImg}
@@ -33,19 +42,7 @@ const Banner: React.FC = () => {
                     </span>
                   </div>
                 </div>
-                <div className="bg-white w-full h-56">
-                  <Image
-                    src={bannerCardImg}
-                    className="w-full h-[180px] object-cover"
-                    alt={""}
-                  />
-                  <div className="bg-primary w-full h-[44px] flex items-center justify-center">
-                    <span className="text-white tracking-wider font-Poppins">
-                      CAPITAL HILLS
-                    </span>
-                  </div>
-                </div>
-              </Link>
+              </div>
             </div>
           </div>
         </div>
